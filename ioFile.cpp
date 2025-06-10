8422687fbd7c1f51479ee9e05014feef8327fcd4
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-10 17:29:05 +0700

    unlimited untuk menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,4 +8,7 @@ int main () {
     //membuka file dalam mode menulis
     ofstream outfile;
     // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
 }
