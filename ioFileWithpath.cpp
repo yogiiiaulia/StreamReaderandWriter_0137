9a190c780e1f8933bf65017bb394305fe675102c
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-10 18:09:48 +0700

    membuka file dalam mode menulis

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -2,3 +2,11 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+    cout << "Masukan nama file : "
+    cin >> NamaFile;
+    //membuka file dalam mode menulis
+}
