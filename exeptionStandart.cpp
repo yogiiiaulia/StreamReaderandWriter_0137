6629b501005d0ea76bca1507c662dd845fd16785
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-10 18:33:33 +0700

    pesan array intejer 3 element

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -3,3 +3,12 @@
 //untuk objek exception yang akan di gunakan
 #include <array>
 //untuk objek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {10, 20, 30};
+        //pesan array intejer 3 element
+    }
+}
