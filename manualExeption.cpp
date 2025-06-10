e92889387fa7c4fb3ac9627e10e6c6ede9f8d91c
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-10 18:47:40 +0700

    jika selain integer makan block ini akan di eksekusi

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -6,5 +6,14 @@ int main()
     try {
         cout << "Selamat Belajar di prodi TI UMY" << endl;
         throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pertanyaan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout <<"Pengecualian akan di eksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer makan block ini akan di eksekusi*/
+        cout << "Default Pengecualian dieksekusi" << endl;
     }
 }
