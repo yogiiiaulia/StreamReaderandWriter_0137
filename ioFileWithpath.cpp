855a21c8584ad7817a9966e7c36d36e75ce31c21
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-10 18:16:51 +0700

    menulis dan memasukan nilai dari 'baris' ke dalam file

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -19,5 +19,7 @@ int main() {
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
     }
 }
